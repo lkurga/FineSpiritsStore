bad18e0a5be879c4ff2b6b666dab9ff8c28220ec
lkurga <50474240+lkurga@users.noreply.github.com>
2021-01-25 20:05:29 -0800

    Update 20170816113956_Initial.cs

--- FineSpiritsStore/Migrations/20170816113956_Initial.cs
+++ FineSpiritsStore/Migrations/20170816113956_Initial.cs
@@ -18,7 +18,10 @@ namespace FineSpiritsStore.Migrations
                     Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Price = table.Column<decimal>(type: "decimal(18, 2)", nullable: false)
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Size = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
                 {

6bd0ebaa1a2b83a36be01fb90529a11ad88f8c07
Vinh Nguyen <vxnguyen@student.rtc.edu>
2021-02-02 21:04:45 -0800

    testing

--- FineSpiritsStore/Models/SeedData.cs
+++ FineSpiritsStore/Models/SeedData.cs
@@ -18,6 +18,7 @@ namespace FineSpiritsStore.Models
             {
                 context.Products.AddRange(
                     //testing change on website
+                    //testing change on Visual studio
    
          new Product
                     {

317a11b0a9e1bf1dcfa09ea2ecd1521b22745e5d
Navudo <xuanvu.do@gmail.com>
2021-02-02 19:50:21 -0800

    Incoporated navigation menu

---
+++ FineSpiritsStore/Models/MinimunAgeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FineSpiritsStore.Models
+{
+    public class MinimunAgeAttribute : ValidationAttribute
+    {
+        int minAge;
+
+        public MinimunAgeAttribute(int minimumAge)
+        {
+            minAge = minimumAge;
+        }
+
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+            if (DateTime.TryParse(value.ToString(), out date))
+            {
+                return date.AddYears(minAge) < DateTime.Now;
+             }
+
+            return false;
+        }
+
+    }
+}

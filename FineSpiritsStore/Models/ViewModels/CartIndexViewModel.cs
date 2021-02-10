6099863697600589291d2e1843d8a4236398b5af
Navudo <xuanvu.do@gmail.com>
2021-02-09 19:23:47 -0800

    Implement shopping cart

---
+++ FineSpiritsStore/Models/ViewModels/CartIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using FineSpiritsStore.Models;
+
+namespace FineSpiritsStore.Models.ViewModels {
+
+    public class CartIndexViewModel {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
